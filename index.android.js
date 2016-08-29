7038eaf5c00e4a40c686a81ad5b6df596fac94f9
Lou George <lgeorge@datacareservices.com>
2016-08-29 16:04:04 -0700

    added a state text to let you know app is working, and made the URL a variable.

--- index.android.js
+++ index.android.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View, Fetch, TouchableOpacity } from 'react-native';
 
+var url = 'http://requestb.in/1n94tai1';
+
 class RestTesting extends Component {
+	constructor(props){
+		super(props);
+		this.state ={
+			action: 'none',
+		};
+	}
 
   	render(){
   		return(
 			<View style={styles.container}>
+				<Text style={styles.title}> Fetcher </Text>
       			<Text style={styles.description}>
       				Try GET or POST from/to RequestBin
       			</Text>
@@ -17,12 +26,16 @@ class RestTesting extends Component {
 	        			<Text> Get </Text>
 	        		</TouchableOpacity>
         		</View>
+        		<Text> {this.state.action} </Text>
+        		<View style={styles.bottomButton}>
+        			<Text style={{color: 'white'}}> {`current URL: ${url}`} </Text>
+        		</View>
       		</View>		
     	);
   	}
 
     _onPressButtonPOST(){
-        fetch('http://requestb.in/1n94tai1', {
+        fetch(url, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -33,16 +46,18 @@ class RestTesting extends Component {
                 secondParam: 'testing',
             }),
         });
+        this.setState({action: 'POST sent!'});
     }
 
     async _onPressButtonGET(){
     	try{
-    		let response = await fetch('http://requestb.in/1n94tai1');
+    		let response = await fetch(url);
     		//let responseJson = await response.json();
     		//return responseJson;
     	} catch (error) {
     		console.error(error);
     	}
+        this.setState({action: 'GET sent!'});
     } 
 }
 
@@ -71,6 +86,23 @@ const styles = StyleSheet.create({
   	marginBottom: 15,
   	color: '#444444'
   },
+
+  bottomButton: {
+  	backgroundColor: '#444444',
+  	position: 'absolute',
+  	alignItems: 'center',
+  	paddingTop: 20,
+  	paddingBottom: 20,
+  	bottom: 0,
+  	left: 0,
+  	right: 0,
+  },
+
+  title: {
+  	fontSize: 25,
+  	color: '#444444',
+  	paddingBottom: 150,
+  }
 });
 
 AppRegistry.registerComponent('RestTesting', () => RestTesting);
